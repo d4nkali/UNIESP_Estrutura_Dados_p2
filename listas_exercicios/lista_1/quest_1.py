e01b32d492789a4234e8f1239a4be9bfac348d9d
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-02-12 13:21:03 -0300

    Adicionando a questão 1

---
+++ listas_exercicios/lista_1/quest_1.py
@@ -0,0 +1,13 @@
+# Questão 1 - Faça um programa que calcule a média de três números inseridos pelo usuário.
+
+# Aluno: Danilo Pereira Viana - P2-B
+
+nota_1 = int(input("Digite a primeira nota: "))
+nota_2 = int(input("Digite a segunda nota: "))
+nota_3 = int(input("Digite a terceira nota: "))
+
+nota_f = (nota_1 + nota_2 + nota_3) / 3
+
+print(f"A media foi de {nota_f}")
+
+# FIM!
