7ef8f4dbf36c47d6041beb46cd370f69248988ab
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-02-17 14:50:14 -0300

    Adicionando questão 2

---
+++ listas_exercicios/lista_1/quest_2.py
@@ -0,0 +1,13 @@
+# Questão 2 - Crie um programa que determine se um número inserido pelo usuário é par ou ímpar.
+
+# Aluno: Danilo Pereira Viana - P2-B
+
+# Pergunta para o usuário digitar um número e armazena em uma variavel 'numero'
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0: # Se o numero for dividido por 2 e o resto igual a zero, então:
+    print(f"{numero} é um número par.") # Imprime que o numero e informa que ele é par
+else: # Senão:
+    print(f"{numero} é um número ímpar.") # Imprime que o numero e informa que ele é impar
+
+# FIM!
