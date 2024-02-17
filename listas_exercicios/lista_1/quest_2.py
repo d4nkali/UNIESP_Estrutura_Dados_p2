b5ec94f0974f9c24876d34aebb879672676d4c8d
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-02-17 15:00:11 -0300

    Update

--- listas_exercicios/lista_1/quest_2.py
+++ listas_exercicios/lista_1/quest_2.py
@@ -5,9 +5,9 @@
 # Pergunta para o usuário digitar um número e armazena em uma variavel 'numero'
 numero = int(input("Digite um número: "))
 
-if numero % 2 == 0: # Se o numero for dividido por 2 e o resto igual a zero, então:
-    print(f"{numero} é um número par.") # Imprime que o numero e informa que ele é par
+if numero % 2 == 0: # Se o número for dividido por 2 e o resto igual a zero, então:
+    print(f"{numero} é um número par.") # Imprime que o número e informa que ele é par
 else: # Senão:
-    print(f"{numero} é um número ímpar.") # Imprime que o numero e informa que ele é impar
+    print(f"{numero} é um número ímpar.") # Imprime que o número e informa que ele é impar
 
 # FIM!
