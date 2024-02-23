81049ae7156e48f56e249488adfbf77162ff2055
d4nkali <d4n_kali@protonmail.com>
2024-02-23 19:44:57 -0300

    Adicionando exemplos 1,2 e 3

---
+++ 23_02_24/exp_3.py
@@ -0,0 +1,11 @@
+# Ordenando uma lista
+
+# Criando uma lista desordenada
+lista = [5, 2, 4, 1, 3]
+
+print("Lista sem ordenacao:", lista)
+
+# Ordenando a lista
+lista.sort()
+
+print("Lista ordenada:", lista)
