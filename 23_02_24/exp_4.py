367a22c759f7f0a7ff71c03244e01990b7ec7e91
d4nkali <d4n_kali@protonmail.com>
2024-02-23 19:54:09 -0300

    Adicionando exemplos 4,5 e 6

---
+++ 23_02_24/exp_4.py
@@ -0,0 +1,6 @@
+# Iterando sobre uma Lista
+
+# Iterando sobre uma lista e imprimindo cada elemento
+lista = ['a', 'b', 'c', 'd', 'e']
+for elemento in lista:
+    print(elemento)
---
+++ 23_02_24/exp_6.py
@@ -0,0 +1,4 @@
+# Retorna o número de elementos de uma lista
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('orange')
