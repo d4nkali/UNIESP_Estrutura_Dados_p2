841dc545271e465f28d813764a1e0c2fa216c6dd
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-02-23 14:44:04 -0300

    Corrigindo e comentando questão 3 e 4

--- listas_exercicios/lista_2/quest_3.py
+++ listas_exercicios/lista_2/quest_3.py
@@ -7,23 +7,26 @@ Aluno: Danilo Pereira Viana - P2-B
 
 '''
 
-class Retangulo:
+class Retangulo: # Cria a classe circulo
 
+    # Com o construtor, define os seguintes atributos (base, altura)
     def __init__(self, base, altura):
 
         self.base = base
         self.altura = altura
         
-    def calcular_area(self):
-        area = self.base * self.altura
-        return area
-    
-perg_base = float(input("Digite a base do retangulo: "))
-perg_altura = float(input("Digite a altura do retangulo:"))
-retangulo_1 = Retangulo(perg_base, perg_altura)
-area_retangulo = retangulo_1.calcular_area()
+    def calcular_area(self): # Cria a função 'calcular_area'
 
-print()
-print(f"A área do retangulo é de {area_retangulo:.2f}")
+        area = self.base * self.altura # Define o calculo da área e armazena em uma variavel
+        return area # Quando a função for acionada, imprime o valor da área
+    
+perg_base = float(input("Digite a base do retangulo: ")) # Pergunta a base e armazena em 'perg_base'
+perg_altura = float(input("Digite a altura do retangulo:")) # Pergunta a altura e armazena em 'perg_altura'
+
+retangulo_1 = Retangulo(perg_base, perg_altura) # Cria o objeto 'retangulo_1'
+area_retangulo = retangulo_1.calcular_area() # Chama a função 'calcular_area'
+
+print() # Espaçamento
+print(f"A área do retangulo é de {area_retangulo:.2f}") # Imprime que vai exebir a área
 
 # FIM!
