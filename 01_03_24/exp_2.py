6adf5719c8ced7e1a48735ea6f787e97ed8e4596
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-03-01 20:42:22 -0300

    Adicionando exemplo 2

---
+++ 01_03_24/exp_2.py
@@ -0,0 +1,15 @@
+vetor = [3,8,7,2,6,1]
+n=len(vetor)
+print(vetor)
+for i in range(n):
+  id_min=i
+  print(i,'<-')
+  for j in range(i+1,n):
+    print(j,'*')
+    if vetor[id_min]>vetor[j]:
+      id_min=j
+  aux=vetor[i]
+  vetor[i]=vetor[id_min]
+  vetor[id_min]=aux
+
+print(vetor)
